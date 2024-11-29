f0ca06bc3c80de1c1811161c88fcfb8d15e1d420
vincent <vincent.nguyennf@gmail.com>
2024-11-28 17:52:17 -0700

    Fixed vite.conf unnecessary import

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
+// import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  // plugins: [react(), TanStackRouterVite()],
+  plugins: [react()],
   base: "https://centenv.github.io/wiki/"
 })
