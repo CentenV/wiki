0c5b74a03c6e1abc5bdf5e8c541a528389f1a568
vincent <vincent.nguyennf@gmail.com>
2024-11-28 18:25:24 -0700

    Changes base URL in vite.conf to new domain

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   // plugins: [react(), TanStackRouterVite()],
   plugins: [react()],
-  base: "https://centenv.github.io/wiki/"
+  base: "https://wiki.centen.dev/"
 })
