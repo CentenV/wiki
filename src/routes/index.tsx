fd2a17e6510a3de26357efe4fb1070f823e5edf7
vincent <vincent.nguyennf@gmail.com>
2024-11-29 17:44:32 -0700

    Root page and layout styled

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,5 +5,11 @@ export const Route = createFileRoute('/')({
 })
 
 function RouteComponent() {
-  return <div className='font-bold text-2xl'>Hello "/"!</div>
+  return (
+    <div className='flex h-full overflow-hidden justify-center items-center'>
+      <div className='flex w-full h-2/3 justify-center'>
+        <span className='font-extrabold text-7xl'>CentenV's Wiki</span>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "wiki-top-bar": "4rem",
+        "wiki-sidebar-full": "18rem",
+        "wiki-logo": "10%"
+      }
+    },
+    colors: {
+      "wiki-background-color": "#121212",
+      "wiki-foreground-color": "#3f3f3f",
+      "wiki-border-color": "#717171",
+    },
+    borderRadius: {
+      "wiki": "0.4rem"
+    },
+    borderWidth: {
+      "wiki": "2px"
+    },
   },
   plugins: [],
 }
