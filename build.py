08599d570774994464c309fa2fa890bf50f27ba5
vincent <vincent.nguyennf@gmail.com>
2024-12-12 23:41:15 -0700

    URL mapping implemented

--- build.py
+++ build.py
@@ -15,29 +15,40 @@ def traverse(folder):
         if entry == "index.tsx":
             full_paths.append(folder[2:])
 
-path_tree = {}
 def map_paths():
+    path_tree = {}
     # Iterate through all the possible full paths
     for path in full_paths:
         # Break down the path
-        path = path.split("/")
+        splitted_path = path.split("/")
 
         # Traverse to locate the proper path
-        current_level = 
-            
+        current_level = path_tree
+        for level in splitted_path:
+            # Add to children
+            if level in current_level.keys():
+                current_level = current_level[level]
             # Create a new node if node does not exist
-
+            else:
+                current_level[level] = {}            
+    return path_tree
 
 def output_ts():
     
     pass
 
 if __name__ == "__main__":
+
+    print("Mapping URLs..........")
     # Set _pages in src as the current directory
     os.chdir("./src/routes/_pages")
     # Traverse and map out all paths
     traverse("./")
     # Convert paths into an object tree
+    tree = map_paths()
+    # Output to JSON file
+    with open("map.json", "w") as map_file:
+        map_file.write(json.dumps(tree))
 
     
     print(full_paths)
