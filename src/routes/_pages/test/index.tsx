3837ba4c1b6247a285af18c1a9807ed47b7d8efe
vincent <vincent.nguyennf@gmail.com>
2024-12-15 01:36:10 -0700

    Configured HTML page titles to dynamically change. Updated page font and favicon

--- src/routes/_pages/test/index.tsx
+++ src/routes/_pages/test/index.tsx
@@ -1,20 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { useEffect, useState } from 'react'
 import { ContentPage } from '../../../components';
 
 export const Route = createFileRoute('/_pages/test/')({
   component: RouteComponent,
 })
 
-function RouteComponent() {
-  const [fileContent, updateFileContent] = useState("");
-  useEffect(() => {
-    async function func() {
-      const content = await fetch("/pages/test.md").then((res) => res.text())
-      updateFileContent(content);
-    }
-    func();
-  }, []);
-
-  return <ContentPage>{fileContent}</ContentPage>
-}
+function RouteComponent() { return <ContentPage pageName='test' /> }
