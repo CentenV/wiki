5d2002d7b6b847959e57a3d63684564010a1e0b9
vincent <vincent.nguyennf@gmail.com>
2024-12-15 18:42:47 -0700

    Clean NextJS project created

--- src/routes/_pages/test/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { ContentPage } from '../../../components';
-
-export const Route = createFileRoute('/_pages/test/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() { return <ContentPage pageName='test' /> }
