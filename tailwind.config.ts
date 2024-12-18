3e1d423cb5cecaea47dab92425b6bcdaa41eab32
vincent <vincent.nguyennf@gmail.com>
2024-12-18 11:21:44 -0700

    Rebuild to a fresh Next project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,23 +8,10 @@ export default {
   ],
   theme: {
     extend: {
-      spacing: {
-        "wiki-sidebar-full": "16rem",
-        "wiki-logo": "10%"
-      }
-    },
-    colors: {
-      "wiki-background-color": "#000000",
-      "wiki-foreground-color": "#151515",
-      "wiki-hud-elements-color": "#353535",
-      "wiki-border-color": "#FFFFFF",
-      "wiki-hud-border-color": "#717171"
-    },
-    borderRadius: {
-      "wiki": "0.4rem"
-    },
-    borderWidth: {
-      "wiki": "1px"
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
     },
   },
   plugins: [],
