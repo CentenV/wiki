45352999758e65b1951e13f8c7ed295db9e39309
vincent <vincent.nguyennf@gmail.com>
2024-12-17 22:22:44 -0700

    Added GitHub actions for NextJS

--- app/(pages)/development/page.tsx
+++ app/(pages)/development/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 import { IntermediatePage } from "@/app/components";
-import { usePathname } from "next/navigation"
 
 export default function Page() {
-    const pathname = usePathname();
-    return <IntermediatePage pageUrl={pathname} backgroundImage={{
+    return <IntermediatePage backgroundImage={{
         url: "development.jpg",
         video: false,
         offset: "-translate-y-32"
--- app/(pages)/test/page.tsx
+++ app/(pages)/test/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 import { ContentPage } from "@/app/components";
 import Content from "./content.mdx";
-import { usePathname } from "next/navigation";
 
 export default function Page() {
-    const url = usePathname();
     return <ContentPage pageName={"test"}><Content /></ContentPage>;
 }
