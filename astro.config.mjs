0560b924584974e87e65037f27cae6a786eb1d09
vincent <vincent.nguyennf@gmail.com>
2024-12-19 15:37:12 -0700

    Updated sidebar. Preparing for adding shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,5 @@ import react from "@astrojs/react";
 export default defineConfig({
   site: "https://wiki.centen.dev",
   base: "/",
-  integrations: [mdx(), tailwind(), react()],
+  integrations: [mdx(), tailwind({ applyBaseStyles: false }), react()],
 });
