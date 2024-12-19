71d0f3b945e70dc9412d7937d0d4a1e762dda698
vincent <vincent.nguyennf@gmail.com>
2024-12-19 14:52:02 -0700

    Fixed content page styling issues related to Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { rehypeHeadingIds } from "@astrojs/markdown-remark"
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 export default defineConfig({
