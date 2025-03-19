72b8799a7f52d9fdfd757b283a67d95f13c7fcc3
vincent <vincent.nguyennf@gmail.com>
2025-03-19 12:14:47 -0600

    Fixed undefined icon error message

--- src/components/PageList.tsx
+++ src/components/PageList.tsx
@@ -5,9 +5,10 @@ export default function PageList({ pages, url }: { pages: Record<string, any>, u
         <div className="flex flex-col gap-2 h-full">
             {
                 Object.entries(pages).map(([pagePath, data]) => {
+                    // URL to the page in the list
                     const destinationUrl = url + pagePath.substring(1, pagePath.length - 4);
+                    // Resolve icon
                     const iconPath = `/src/assets/icons/${data.frontmatter["icon"]}`
-                    if (!images[iconPath]) { console.error(`Icon "${iconPath}" for ${data.frontmatter["icon"]} not found for the page list`); }
                     return (
                         <a href={destinationUrl} className="flex flex-row p-2 pl-4 border-wiki-default border-wiki-background-color hover:border-wiki-hud-border-color rounded-wiki gap-2">
                             {(images[iconPath]) ? <img src={images[iconPath].default.src} alt={""} className="invert" /> : <span></span>}
