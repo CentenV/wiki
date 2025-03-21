749d962569e068207af95ae30df31cbf5b381b43
vincent <vincent.nguyennf@gmail.com>
2025-03-21 12:26:07 -0600

    Implemented responsiveness

--- src/components/PageList.tsx
+++ src/components/PageList.tsx
@@ -10,7 +10,7 @@ export default function PageList({ pages, url }: { pages: Record<string, any>, u
                     // Resolve icon
                     const iconPath = `/src/assets/icons/${data.frontmatter["icon"]}`
                     return (
-                        <a href={destinationUrl} className="flex flex-row p-2 pl-4 border-wiki-default border-wiki-background-color hover:border-wiki-hud-border-color rounded-wiki gap-2">
+                        <a href={destinationUrl} className="flex flex-row p-2 pl-4 border-wiki-default hover:border-wiki-hud-border-color border-wiki-hud-border-color lg:border-wiki-background-color rounded-wiki gap-2">
                             {(images[iconPath]) ? <img src={images[iconPath].default.src} alt={""} className="invert" /> : <span></span>}
                             <div className="col-start-2 col-span-1">{data.frontmatter.title}</div>
                         </a>
